91227dd0f1bb7a3a7a80ffe98cea75b7e25e9690
my-official-nerd-friend <218838026+my-official-nerd-friend@users.noreply.github.com>
2025-07-15 21:33:31 +0200

    constants : remove comment

--- common_utils/constants.py
+++ common_utils/constants.py
@@ -2,9 +2,6 @@ import os
 
 from common_utils.classes import HelpCommand
 
-
-#from common_utils.classes.help_command import HelpCommand
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 TELEGRAM_CHANNEL = os.getenv("TELEGRAM_CHANNEL")
 COMMANDS = {
